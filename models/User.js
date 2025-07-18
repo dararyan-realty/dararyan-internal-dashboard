625f5c289bad82d7a263b527fd1e00e393ecacd8
dararyan-realty <kuwait.re@gmail.com>
2025-07-18 19:25:02 +0330

    Added User model

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  phone: { type: String },
+  role: { type: String, enum: ['admin', 'employee'], default: 'employee' },
+  permissions: [{ permission: String, temporary: Boolean, expiryDate: Date }] // For custom permissions, including temporary
+}, { timestamps: true });
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
