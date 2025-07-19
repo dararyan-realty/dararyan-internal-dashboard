08b35c90b49d7cf56b30ee2fcb540e05375b93a6
dararyan-realty <kuwait.re@gmail.com>
2025-07-19 21:33:33 +0330

    Added static serving for frontend and root route

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
